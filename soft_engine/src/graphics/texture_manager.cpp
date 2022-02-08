911ff7ecb859cfb27d3f76a409682220264c25ef
Hen Halpert <henhhalpert@gmail.com>
2022-02-08 20:11:25 +0200

    DrawFrame function added

--- soft_engine/src/graphics/texture_manager.cpp
+++ soft_engine/src/graphics/texture_manager.cpp
@@ -38,6 +38,20 @@ void TextureManager::Draw(std::string id, int x, int y, int width, int height, S
 
 }
 
+void TextureManager::DrawFrame(std::string id, int x, int y, int w, int h, int row, int frame, SDL_RendererFlip flip)
+{
+	SDL_Rect srcRect = { w*frame, h*(row-1), w, h};
+	SDL_Rect destRect = { x, y, w, h };
+	SDL_RenderCopyEx(
+		Engine::GetInstance()->GetRenderer(),
+		m_TextureMap[id],
+		&srcRect,
+		&destRect,
+		0,
+		nullptr,
+		flip);
+}
+
 void TextureManager::Drop(std::string id)
 {
 	//destroy texture
