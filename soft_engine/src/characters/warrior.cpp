94b3f93323a7c83688fb62c40b58265cda91ffe0
Hen Halpert <henhhalpert@gmail.com>
2022-02-17 15:28:13 +0200

    Warrior character can now move forward / backwards using keyboard input from user

--- soft_engine/src/characters/warrior.cpp
+++ soft_engine/src/characters/warrior.cpp
@@ -1,29 +1,51 @@
 #include "warrior.h"
 #include "../graphics/texture_manager.h"
 #include "SDL.h"
-
+#include "../inputs/input.h"
 
 Warrior::Warrior(Properties* props)
 	: Character(props)
 {
 	m_RigidBody = new RigidBody();
 	m_Animation = new Animation();
-	m_Animation->SetProps(props->m_TextureID, 1, 12, 50, SDL_FLIP_HORIZONTAL);
+	m_Animation->SetProps(props->m_TextureID, 1, 8, 60, SDL_FLIP_NONE);
 }
 
 void Warrior::Draw()
 {
-
-	m_Animation->Draw(m_Transform->X, m_Transform->Y, m_Width, m_Height); 
+	m_Animation->Draw(m_Transform->X, m_Transform->Y, m_Width, m_Height);
 }
 
 void Warrior::Update(float dt)
 {	
-	m_RigidBody->Update(0.3);
-	m_RigidBody->ApplyForceX(6);
+	m_RigidBody->ResetForce();
+	m_Width = 64;
+
+	if(m_Flip == SDL_FLIP_HORIZONTAL)
+		m_Animation->SetProps("player", 1, 15, 40, SDL_FLIP_HORIZONTAL);
+	else
+		m_Animation->SetProps("player", 1, 15, 40, SDL_FLIP_NONE);
+	
+	//move forward
+	if (Input::GetInstance()->GetKeyDown(SDL_SCANCODE_D))
+	{
+		m_RigidBody->ApplyForceX(5 * FORWARD);
+		m_Width = 96;
+		m_Flip = SDL_FLIP_NONE;
+		m_Animation->SetProps("player_run", 1, 8, 60, SDL_FLIP_NONE);
+	}
+
+	//move backwards
+	if (Input::GetInstance()->GetKeyDown(SDL_SCANCODE_A))
+	{
+		m_RigidBody->ApplyForceX(5 * BACKWARDS);
+		m_Width = 96;
+		m_Flip = SDL_FLIP_HORIZONTAL;
+		m_Animation->SetProps("player_run", 1, 8, 60, SDL_FLIP_HORIZONTAL);
+	}
 
 	m_Transform->TranslateX(m_RigidBody->Position().X);
-	m_Transform->TranslateY(m_RigidBody->Position().Y);
+	m_RigidBody->Update(0.5);
 	m_Animation->Update();
 }
 
